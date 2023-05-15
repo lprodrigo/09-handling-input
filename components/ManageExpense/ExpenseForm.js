368cb899d9ad9eb64cd76bf9328806b8cc66c785
Rodrigo <rodrigo.lopez@focusservices.com>
2023-05-15 15:37:29 -0600

    feat: add input validation

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,16 +1,40 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
-import Input from "./Input";
 import { useState } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import Input from "./Input";
+import Button from "../UI/Button";
 
-const ExpenseForm = () => {
+const ExpenseForm = ({
+  onCancel,
+  onSubmit,
+  submitButtonLabel,
+  defaultValues,
+}) => {
   const [inputValues, setInputValues] = useState({
-    amount: "",
-    date: "",
-    description: "",
+    amount: defaultValues ? defaultValues.amount.toString() : "",
+    date: defaultValues ? defaultValues.date.toISOString().slice(0, 10) : "",
+    description: defaultValues ? defaultValues.description : "",
   });
 
   const inputChangeHandler = (inputIdentifier, enteredText) => {
     setInputValues((prev) => ({ ...prev, [inputIdentifier]: enteredText }));
+  };
+
+  const submitHandler = () => {
+    const expenseData = {
+      amount: +inputValues.amount,
+      date: new Date(inputValues.date),
+      description: inputValues.description,
+    };
+
+    const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+    const dateIsValid = expenseData.date.toString() !== "Invalid Date";
+    const descriptionIsValid = expenseData.description.trim().length > 0;
+
+    if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+      return Alert.alert("Invalid input", "Please check your input values");
+    }
+
+    onSubmit(expenseData);
   };
 
   return (
@@ -45,6 +69,14 @@ const ExpenseForm = () => {
           value: inputValues.description,
         }}
       />
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={onCancel}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={submitHandler}>
+          {submitButtonLabel}
+        </Button>
+      </View>
     </View>
   );
 };
@@ -63,6 +95,15 @@ const styles = StyleSheet.create({
     color: "white",
     textAlign: "center",
     marginVertical: 24,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
 });
 
